115546ae5e56934bc8445308ef61f3d38ba2b50d
Vel00 <v_denissov@bk.ru>
2022-12-03 16:49:40 +0300

    Add test for multiple purchases

--- shop/tests/test_views.py
+++ shop/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client, RequestFactory
 from shop.views import PurchaseCreate
-from shop.models import Product
+from shop.models import Product, Purchase
 
 factory = RequestFactory()
 
@@ -14,17 +14,30 @@ class IndexTestCase(TestCase):
 
 class PurchaseCreateTestCase(TestCase):
     def setUp(self):
-        yacht = Product.objects.create(name="Яхта", price=90000000, amount=1)
-        self.id = yacht.id
+        painting = Product.objects.create(name="Картина", price=100000, amount=5)
+        self.painting_id = painting.id
 
-        self.request_data = {
-            "product": self.id,
+        yacht = Product.objects.create(name="Яхта", price=90000000, amount=1)
+        self.yacht_id = yacht.id
+
+        self.request_data_yacht = {
+            "product": self.yacht_id,
             "person": "Dimon",
             "address": "LA"
         }
 
+        self.request_data_painting = {
+            "product": self.painting_id,
+            "person": "Timon",
+            "address": "NY"
+        }
+
+    def tearDown(self):
+        Product.objects.all().delete()
+        Purchase.objects.all().delete()
+
     def test_purchase(self):
-        request = factory.post(f"/buy/{self.id}", data=self.request_data)
+        request = factory.post(f"/buy/{self.yacht_id}", data=self.request_data_yacht)
         response = PurchaseCreate.as_view()(request)
 
         self.assertEqual(response.status_code, 200)
@@ -34,3 +47,11 @@ class PurchaseCreateTestCase(TestCase):
 
         response = self.client.get('/')
         self.assertTrue("Нет в наличии" in response.content.decode())
+
+    def test_multiple_purchase(self):
+        request = factory.post(f"/buy/{self.painting_id}", data=self.request_data_painting)
+        for i in range(4):
+            PurchaseCreate.as_view()(request)
+
+        painting = Product.objects.get(name="Картина")
+        self.assertEqual(painting.amount, 1)
